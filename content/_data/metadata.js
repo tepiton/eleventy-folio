a5c48ebad208e605cdbcc0090a34e697a54f028c
Philip Borenstein <pborenstein@gmail.com>
2026-09-10 00:30:56 -0400

    add the mimeo template

--- content/_data/metadata.js
+++ content/_data/metadata.js
@@ -1,13 +1,11 @@
 export default {
 	title: "Folio",
 	subtitle: "A starter for chaptered narrative sites",
-	url: "https://orobia.net/",
+	url: "https://example.com/",
 	language: "en",
 	description: "An Eleventy starter for chaptered, long-form literary projects.",
 	author: {
 		name: "Your Name",
-		email: "youremailaddress@example.com",
-		url: "https://orobia.net/"
 	},
 	image: "",
 }
